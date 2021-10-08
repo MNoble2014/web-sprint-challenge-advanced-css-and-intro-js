2f49bcbb4e62a1acfd582f5cf7cbe1f43af3a770
MNoble2014 <michealsimpson2013@gmail.com>
2021-10-08 13:46:33 -0400

    Task 5 test passing

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { arrayTypeAnnotation } = require("@babel/types");
+
 const artists = [
   {
     "id": 0,
@@ -245,10 +247,9 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(/*Your Code Here*/) {
-  /*Your Code Here*/
+function get20s() {
+ 
 }
-
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -260,10 +261,11 @@ Use removeArtist to do the following:
 
 For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function removeArtist(array) {
+  array.pop();
+  return(artists.length)
 }
-
+console.log('task 5', removeArtist(artists, 0));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use addArtist to do the following: 
